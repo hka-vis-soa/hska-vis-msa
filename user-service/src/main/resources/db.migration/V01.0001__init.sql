0915b98aed62debe93a0337614ed11327b1ab042
dnszlr <dennis.zhukki@gmail.com>
2023-04-18 11:07:39 +0200

    flyway changes

--- user-service/src/main/resources/db.migration/V01.0001__init.sql
+++ user-service/src/main/resources/db.migration/V01.0001__init.sql
@@ -4,5 +4,5 @@ CREATE TABLE IF NOT EXISTS userservice.users
 (
     ID                  SERIAL PRIMARY KEY,
     FIRSTNAME           VARCHAR(20)  NOT NULL,
-    LASTNAME            VARCHAR(20)  NOT NULL,
+    LASTNAME            VARCHAR(20)  NOT NULL
 );
