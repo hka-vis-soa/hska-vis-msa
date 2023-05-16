d124c55f3fd28a5ce1d7e882624e3ce23602ca77
Innoberger <fynn.arnold@pm.me>
2023-05-16 14:45:41 +0200

    product id is now returned by default

--- product-service/src/main/java/com/hska/eshop/productservice/model/Product.java
+++ product-service/src/main/java/com/hska/eshop/productservice/model/Product.java
@@ -30,6 +30,14 @@ public class Product {
         this.category_id = category_id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
     public String getName() {
         return name;
     }
