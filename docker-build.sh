ac10b81b89361a960014cd6e6b7701e619e821b4
Innoberger <fynn.arnold@pm.me>
2023-05-23 14:34:34 +0200

    prepared kubernetes deploy

--- docker-build.sh
+++ docker-build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-if [ -z "$DOCKER_ACCOUNT" ]; then
-    echo "Variable DOCKER_ACCOUNT required. Use 'export DOCKER_ACCOUNT=my_account'"
-    exit 1
-fi
+#if [ -z "$DOCKER_ACCOUNT" ]; then
+#    echo "Variable DOCKER_ACCOUNT required. Use 'export DOCKER_ACCOUNT=my_account'"
+#    exit 1
+#fi
 
 docker build --tag=category-service category-service
-docker tag category-service $DOCKER_ACCOUNT/category-service:latest
-docker push $DOCKER_ACCOUNT/category-service
+#docker tag category-service $DOCKER_ACCOUNT/category-service:latest
+#docker push $DOCKER_ACCOUNT/category-service
 
 docker build --tag=product-service product-service
-docker tag product-service $DOCKER_ACCOUNT/product-service:latest
-docker push $DOCKER_ACCOUNT/product-service
+#docker tag product-service $DOCKER_ACCOUNT/product-service:latest
+#docker push $DOCKER_ACCOUNT/product-service
--- kubernetes-deploy.sh
+++ kubernetes-deploy.sh
@@ -1,11 +1,24 @@
 #!/bin/sh
 
-if [ -z "$DOCKER_ACCOUNT" ]; then
-    echo "Variable DOCKER_ACCOUNT required. Use 'export DOCKER_ACCOUNT=my_account'"
-    exit 1
-fi
+#if [ -z "$DOCKER_ACCOUNT" ]; then
+#    echo "Variable DOCKER_ACCOUNT required. Use 'export DOCKER_ACCOUNT=my_account'"
+#    exit 1
+#fi
 
-kubectl create deployment category --image=docker.io/$DOCKER_ACCOUNT/category-service:latest --port=8000
-kubectl expose deployment category --port 8000 --port 5432
-kubectl create deployment product --image=docker.io/$DOCKER_ACCOUNT/product-service:latest --port=8001
-kubectl expose deployment product --port 8001 --port 5432
+kubectl create deployment postgres --image=postgres:latest --port 5432
+kubectl expose deployment postgres --target-port 5432
+
+kubectl create deployment pgadmin --image=dpage/pgadmin4:latest --port 80
+kubectl expose deployment pgadmin --target-port 5050
+
+kubectl create deployment category-service --image=hska-vis-msa-category-service:latest --port 8080
+kubectl expose deployment category-service --target-port 8000
+
+kubectl create deployment product-service --image=hska-vis-msa-product-service:latest --port 8080
+kubectl expose deployment product-service --target-port 8001 
+
+kubectl create deployment web-shop-db-image --image=mavogel/hska-vis-web-shop-db-image:latest --port 3306
+kubectl expose deployment web-shop-db-image --target-port 3306
+
+kubectl create deployment legacywebshop --image=mavogel/hska-vis-legacywebshop:latest --port 8080
+kubectl expose deployment legacywebshop --target-port 8888
