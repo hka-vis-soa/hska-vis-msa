68d54a3a31bad0b07c77f98a0bb4f1e9f2c61c4e
Innoberger <fynn.arnold@pm.me>
2023-05-23 14:08:55 +0200

    var check

--- docker-build.sh
+++ docker-build.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [ -z "$DOCKER_ACCOUNT" ]; then
+    echo "Variable DOCKER_ACCOUNT required. Use 'export DOCKER_ACCOUNT=my_account'"
+    exit 1
+fi
+
 docker build --tag=category-service category-service
 docker tag category-service $DOCKER_ACCOUNT/category-service:latest
 docker push $DOCKER_ACCOUNT/category-service
--- kubernetes-deploy.sh
+++ kubernetes-deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+
+if [ -z "$DOCKER_ACCOUNT" ]; then
+    echo "Variable DOCKER_ACCOUNT required. Use 'export DOCKER_ACCOUNT=my_account'"
+    exit 1
+fi
+
 kubectl create deployment category --image=docker.io/$DOCKER_ACCOUNT/category-service:latest --port=8000
 kubectl expose deployment category --port 8000 --port 5432
 kubectl create deployment product --image=docker.io/$DOCKER_ACCOUNT/product-service:latest --port=8001
